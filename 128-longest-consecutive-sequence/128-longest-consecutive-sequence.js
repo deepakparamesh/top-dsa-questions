5e657bdf8841b3a2727047e7ca4c111e0203ae5e
deepak kumar <deepakparamesh007@gmail.com>
2022-07-22 23:39:30 +0530

    Time: 716 ms (17.57%), Space: 58.2 MB (43.92%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -1,41 +1,75 @@
+function longestConsecutive(nums) {
+    if (!nums.length) {
+        return 0;
+    }
+
+    const map = Object.create(null);
+    let max = 0;
+
+    for (const num of nums) {
+        if (num in map) {
+            continue;
+        }
+
+        const prev = num - 1;
+        const next = num + 1;
+        let len = 1;
+
+        if (prev in map) {
+            if (next in map) {
+                len += map[prev] + map[next];
+                map[prev - map[prev] + 1] = len;
+                map[next + map[next] - 1] = len;
+            } else {
+                len += map[prev];
+                ++map[prev - map[prev] + 1];
+            }
+        } else if (next in map) {
+            len += map[next];
+            ++map[next + map[next] - 1];
+        }
+        map[num] = len;
+        max = Math.max(max, len);
+    }
+
+    return max;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Linear Search With A Hash Set
+// Time: O(n)
+// Space: O(n)
+// This solution does three passes over the `nums` array. A first pass to
+// setup the hash set. A second pass to find the numbers that mark the
+// beginning of a sequence. A third pass to calculate the length of each
+// sequence. The nested `while` loop does not cause quadratic calculations as
+// it is only initiated on the first number of each sequence.
+//////////////////////////////////////////////////////////////////////////////
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function(nums) {
-     if (!nums.length) {
-       return 0;
-   }
-   
-   const map = Object.create(null);
-   let max = 0;
-   
-   for (const num of nums) {
-       
-       if (num in map) {
-	   continue;
-       }
-       
-       const prev = num - 1;
-       const next = num + 1;
-       let len = 1;
-       
-       if (prev in map) {
-	   if (next in map) {
-	       len += map[prev] + map[next];
-	       map[prev - map[prev] + 1] = len;
-	       map[next + map[next] - 1] = len;
-	   } else {
-	       len += map[prev];
-	       ++map[prev - map[prev] + 1];
-	   }
-       } else if (next in map) {
-	   len += map[next];
-	   ++map[next + map[next] - 1];
-       }
-       map[num] = len;
-       max = Math.max(max, len);
-   }
-   
-   return max;
-};
+function longestConsecutive(nums) {
+    const set = new Set(nums);
+    let max = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+
+        if (set.has(num - 1)) {
+            continue;
+        }
+
+        let currentMax = 1;
+        while (set.has(num + currentMax)) {
+            currentMax++;
+        }
+
+        if (currentMax > max) {
+            max = currentMax;
+        }
+    }
+
+    return max;
+}
