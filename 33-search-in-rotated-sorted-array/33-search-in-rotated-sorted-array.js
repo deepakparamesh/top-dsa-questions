ca94d509c340c1d742d27cdce35bfb42d5492366
deepak kumar <deepakparamesh007@gmail.com>
2022-08-02 21:02:32 +0530

    Time: 87 ms (56.76%), Space: 41.4 MB (99.68%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -4,41 +4,31 @@
  * @return {number}
  */
 var search = function(nums, target) {
-     let left = 0;
-  let right = nums.length - 1;
+    let left=0;
+    let right=nums.length-1;
     
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    
-    if (nums[mid] === target) {
-      return mid;
-    }
-    
-    // Checking if the left side is sorted
-    if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target <= nums[mid]) {
-        // thus target is in the left
-        right = mid - 1;
+    while(left<=right){
+        let mid = Math.floor((left+right)/2);
         
-      } else {
-        // thus target is in the right
-        left = mid + 1;
-      }
-    } 
-    
-    // Otherwise, the right side is sorted
-    else {
-      if (nums[mid] <= target && target <= nums[right]) {
-        // thus target is in the right
-        left = mid + 1;
-
-      } else {
-        // thus target is in the left
-        right = mid - 1;
-      }
+        if(nums[mid] === target) return mid;
+        // we are in the left sorted portion
+        if(nums[left]<=nums[mid]){
+            if(target>=nums[left] && target <= nums[mid]){
+                right = mid-1;
+            } else {
+                left = mid+1;
+            }
+        }
+        // we are in the right sorted portion
+        else {
+            if(target>=nums[mid] && target <= nums[right]){
+                left=mid+1;
+            } else{
+                right=mid-1;
+            }
+        }
+        
     }
     
-  }
-    
-  return -1;
+    return -1;
 };
