76517150bc06ff0f4afcaeab2af9ee863585ae98
deepak kumar <deepakparamesh007@gmail.com>
2022-08-04 23:26:14 +0530

    Time: 147 ms (20.35%), Space: 50.9 MB (20.81%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,13 +3,12 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    maxSub = nums[0];
-    curSum = 0;
-    
-    for(num of nums){
-        if(curSum < 0) curSum = 0;
-        curSum += num;
-        maxSub = Math.max(maxSub, curSum);
-    }
-    return maxSub;
+maxSub = nums[0];
+curSum = 0;
+for(num of nums){
+    if(curSum < 0) curSum = 0;
+    curSum += num;
+    maxSub = Math.max(maxSub, curSum);
+}
+return maxSub;
 };
