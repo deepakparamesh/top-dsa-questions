63bafe419dde307de8051622809466e41ed4b7f2
deepak kumar <deepakparamesh007@gmail.com>
2022-08-06 13:25:53 +0530

    Time: 58 ms (97.20%), Space: 42.3 MB (50.22%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -12,7 +12,9 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
+    // structure comparision
     if(!p && !q) return true;
+    // value comparision
     if(!p || !q || p.val !== q.val) return false;
-    return (isSameTree(p.left,q.left) && isSameTree(p.right, q.right));
+    return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
 };
