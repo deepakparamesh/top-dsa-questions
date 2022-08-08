0a2021a80a65b4eb2b73ff2dc3ba8b2bfe63a6e4
deepak kumar <deepakparamesh007@gmail.com>
2022-08-09 00:53:04 +0530

    Time: 626 ms (28.17%), Space: 57.5 MB (65.04%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -3,23 +3,23 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    if(nums.length === 0) return 0;
+    if(!nums.length) return 0;
     
     let hashSet = new Set(nums);
     let max = 0;
     
     for(let num of nums){
-        
         if(hashSet.has(num-1)) {
             continue;
         }
         
         let currentMax = 1;
         while(hashSet.has(num+currentMax)) {
-            currentMax += 1;
+            currentMax +=1;
         }
         
-        if(currentMax > max) max = currentMax;
+        if(max < currentMax) max = currentMax;
     }
+    
     return max;
 };
