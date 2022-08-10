016be62e4291a5d8308c3c32c2a247e7581e8dcc
deepak kumar <deepakparamesh007@gmail.com>
2022-08-10 23:10:07 +0530

    Time: 85 ms (97.02%), Space: 46.9 MB (72.11%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -1,51 +1,41 @@
-var lengthOfLongestSubstring = function (str) {
-    const hash = {};
-    let start = 0;
-    let max = 0;
+// var lengthOfLongestSubstring = function (str) {
+//     const hash = {};
+//     let start = 0;
+//     let max = 0;
 
-    for (let i = 0; i < str.length; i++) {
-        let rightChar = str[i];
+//     for (let i = 0; i < str.length; i++) {
+//         let rightChar = str[i];
 
-        if (!(rightChar in hash)) hash[rightChar] = 0;
-        hash[rightChar] += 1;
+//         if (!(rightChar in hash)) hash[rightChar] = 0;
+//         hash[rightChar] += 1;
 
-        while (hash[rightChar] > 1) {
-            let leftChar = str[start];
-            start += 1;
+//         while (hash[rightChar] > 1) {
+//             let leftChar = str[start];
+//             start += 1;
 
-            if (leftChar in hash) hash[leftChar] -= 1;
+//             if (leftChar in hash) hash[leftChar] -= 1;
+//         }
+//         max = Math.max(max, i - start + 1);
+//     }
+//     return max;
+// };
+
+var lengthOfLongestSubstring = function(s) {
+    map = new Map()
+    l = 0
+    res = 0
+    
+    for (let r=0; r < s.length; r++) {
+        
+        while (map.has(s[r])) {
+            map.delete(s[l])
+            l++
         }
-        max = Math.max(max, i - start + 1);
+    
+        map.set(s[r], s[r])
+        res = Math.max(res, r-l + 1)
+        
     }
-    return max;
+    return res
+    
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
