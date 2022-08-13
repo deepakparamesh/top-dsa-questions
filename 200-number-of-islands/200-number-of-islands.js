41f31be310e89c02cc085d0478d6ad8a3560138e
deepak kumar <deepakparamesh007@gmail.com>
2022-08-13 13:08:44 +0530

    Time: 129 ms (54.77%), Space: 44.3 MB (98.75%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -2,34 +2,35 @@
  * @param {character[][]} grid
  * @return {number}
  */
-function dfs(grid, i, j) {
-    if (
-        i < 0 ||
-        i >= grid.length ||
-        j < 0 ||
-        j >= grid[0].length ||
-        grid[i][j] === '0'
-    ) {
-        return;
-    }
 
-    grid[i][j] = '0';
-    dfs(grid, i + 1, j);
-    dfs(grid, i - 1, j);
-    dfs(grid, i, j + 1);
-    dfs(grid, i, j - 1);
+function dfs(grid, row, col) {
+    if(row < 0 || row >= grid.length ||
+       col < 0 || col >= grid[0].length ||
+       grid[row][col] === "0") {
+        return ;
+    }    
+    
+    grid[row][col] = "0";
+    dfs(grid, row-1, col);
+    dfs(grid, row+1, col);
+    dfs(grid, row, col-1);
+    dfs(grid, row, col+1);
 }
 
 var numIslands = function (grid) {
-    let count = 0;
-
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] === '1') {
-                count += 1;
+    if(!grid.length) return 0;
+    
+    let islands = 0;
+    
+    for(let i=0; i< grid.length ; i++) {
+        for(let j=0; j< grid[0].length; j++){
+            if(grid[i][j] === "1"){
+                islands += 1;
                 dfs(grid, i, j);
             }
         }
     }
-    return count;
+    return islands;
+    
+    
 };
