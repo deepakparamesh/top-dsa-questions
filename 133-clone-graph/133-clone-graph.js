3c7189c1fe3495c1c9a78c0aeafd846c62eb0fdf
deepak kumar <deepakparamesh007@gmail.com>
2022-08-14 11:02:43 +0530

    Time: 105 ms (32.47%), Space: 43.4 MB (93.29%) - LeetHub

--- 133-clone-graph/133-clone-graph.js
+++ 133-clone-graph/133-clone-graph.js
@@ -13,18 +13,19 @@
 var cloneGraph = function(node) {
     let visited = {};
     let dfs = (node) => {
-        if (!node) {
+        if(!node){
             return node;
         }
-        if (visited[node.val]) {
+        if(visited[node.val]) {
             return visited[node.val];
         }
+        
         let copy = new Node(node.val);
         visited[node.val] = copy;
-        node.neighbors.forEach((n) => {
-            copy.neighbors.push(dfs(n));
+        node.neighbors.forEach((neighbor) => {
+            copy.neighbors.push(dfs(neighbor));
         });
         return copy;
-    };
+    }
     return dfs(node);
 };
