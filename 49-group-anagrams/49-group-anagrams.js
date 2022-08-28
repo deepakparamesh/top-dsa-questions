4ebfcdbfa3ef6b48f665647f73203da4635d5614
deepak kumar <deepakparamesh007@gmail.com>
2022-08-28 18:04:14 +0530

    Time: 444 ms (5.95%), Space: 63 MB (5.03%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -3,14 +3,32 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
+    
+    // Version 1 which will cost us O(m*nlogn)
+    // let hashMap = {};
+    // let result = [];   
+    // for(let i=0; i< strs.length; i++){
+    //     let sortedString = strs[i].split('').sort().join('');
+    //     if(!(sortedString in hashMap)){
+    //         hashMap[sortedString] = [];
+    //     }
+    //     hashMap[sortedString].push(strs[i]);
+    // }
+    // for(let key in hashMap) {
+    //     result.push(hashMap[key]);
+    // }  
+    // return result;
+    
     let hashMap = {};
     let result = [];   
     for(let i=0; i< strs.length; i++){
-        let sortedString = strs[i].split('').sort().join('');
-        if(!(sortedString in hashMap)){
-            hashMap[sortedString] = [];
+        let hashKey = new Array(26).fill(0);
+        for(let char of strs[i]) {
+            hashKey[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+            hashKey.toString();
         }
-        hashMap[sortedString].push(strs[i]);
+        if(!(hashKey in hashMap)) hashMap[hashKey] = [];
+        hashMap[hashKey].push(strs[i]);
     }
     for(let key in hashMap) {
         result.push(hashMap[key]);
