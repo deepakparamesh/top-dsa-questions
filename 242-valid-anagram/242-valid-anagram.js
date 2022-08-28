18c62d6f6124ca8649e2744fdcd1b2f1eb3a7083
deepak kumar <deepakparamesh007@gmail.com>
2022-08-28 13:51:43 +0530

    Time: 132 ms (43.07%), Space: 43.4 MB (74.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -5,19 +5,16 @@
  */
 var isAnagram = function(s, t) {
     if(s.length !== t.length) return false;
-    
-    let sHashMap = {};
-    let tHashMap = {};
-    for(let i=0; i<s.length; i++){
-        sHashMap[s[i]] = sHashMap[s[i]] ? ++sHashMap[s[i]] : 1;
-        tHashMap[t[i]] = tHashMap[t[i]] ? ++tHashMap[t[i]] : 1;
-    };
-    
-    for(let char in sHashMap){
-        if(sHashMap[char] !== tHashMap[char])
-            return false;
+    let sHash = {};
+    let tHash = {};
+    for(let i=0; i<s.length; i++) {
+        sHash[s[i]] = sHash[s[i]] ? sHash[s[i]] += 1 : 1;
+        tHash[t[i]] = tHash[t[i]] ? tHash[t[i]] += 1 : 1;
     }
-    
+    for(let char in sHash) {
+        if(sHash[char] !== tHash[char]) {
+            return false;
+        }
+    }
     return true;
-    
 };
