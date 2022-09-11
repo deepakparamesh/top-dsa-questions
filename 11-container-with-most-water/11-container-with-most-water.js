6b505b8ea46a773049c5febe950afd6b145e154f
deepak kumar <deepakparamesh007@gmail.com>
2022-09-11 09:06:19 +0530

    Time: 155 ms (11.16%), Space: 50.1 MB (18.40%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -2,23 +2,14 @@
  * @param {number[]} height
  * @return {number}
  */
-
-// Lesson learnt
-
-// Lesson 1: Always be clear whether we should go with index or indices.
-// Initially you went with indices and that messed up the whole problem.
-
-// Lesson 2: Carelessness with the left pointer and right pointer movement condition validation.
-// used if(left < height) instead of if(height[left] < height[right])
 var maxArea = function(height) {
-    let left=0, right=height.length-1;
-    let maxArea = 0;
+    let [left, right] = [0, height.length-1];
+    let max = 0;
     while(left < right){
-        let tempArea = Math.min(height[left], height[right]) * (right - left);
-        if(tempArea > maxArea) maxArea = tempArea;
-        if(height[left] < height[right]) left++;
+        let area = (right - left) * Math.min(height[left], height[right]);
+        max = (area > max) ? area : max;
+        if(height[left] <= height[right]) left++;
         else if(height[right] < height[left]) right--;
-        else left++;
     }
-    return maxArea;
+    return max;
 };
