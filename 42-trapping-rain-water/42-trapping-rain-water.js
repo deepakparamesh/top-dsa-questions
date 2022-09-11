f2853e066620130e660d13295880b2701825db53
deepak kumar <deepakparamesh007@gmail.com>
2022-09-11 17:06:25 +0530

    Time: 60 ms (99.42%), Space: 44.6 MB (39.12%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.js
+++ 42-trapping-rain-water/42-trapping-rain-water.js
@@ -3,40 +3,32 @@
  * @return {number}
  */
 var trap = function(height) {
-    let [ left, right ] = [ 0, (height.length - 1) ];
-    let [ maxLeft, maxRight, area ] = [ 0, 0, 0 ];
-    
-    while (left < right) {
-        const [ leftHeight, rightHeight ] = getHeights(height, left, right);
-        const [ leftWindow, rightWindow ] = getWindows(height, left, maxLeft, right, maxRight);
+    let [left, right] = [0, height.length-1];
+    let [maxLeft, maxRight] = [height[left], height[right]];
+    let area = 0;
 
-        const isRightGreater = leftHeight <= rightHeight;
-        if (isRightGreater) {
-            if (hasNewMax(maxLeft, leftHeight)) maxLeft = leftHeight;
-            else area += leftWindow;
-
-            left++;
-        }
-
-        const isRightLess = rightHeight < leftHeight;
-        if (isRightLess) {
-            if (hasNewMax(maxRight, rightHeight)) maxRight = rightHeight;
-            else area += rightWindow;
-
-            right--;
+    while(left < right){
+        [maxLeft, maxRight] = calculateMax(height, left, right, maxLeft, maxRight);
+        let tempArea = 0;
+        if(maxLeft <= maxRight) {
+             left += 1;
+             tempArea = Math.min(maxLeft, maxRight) - height[left];
+             tempArea = (tempArea < 0) ? 0 : tempArea;
+             area += tempArea;
+        } else if(maxRight < maxLeft) {
+            right -= 1;
+            tempArea = Math.min(maxLeft, maxRight) - height[right];
+            tempArea = (tempArea < 0) ? 0 : tempArea;
+            area += tempArea;
         }
     }
-
+    
     return area;
+    
 };
 
-const hasNewMax = (max, height) => max < height;
-
-const getHeights = (height, left, right) => [ height[left], height[right] ];
-
-const getWindows = (height, left, maxLeft, right, maxRight) => {
-    const [ leftHeight, rightHeight ] = getHeights(height, left, right);
-    const [ leftWindow, rightWindow ] = [ (maxLeft - leftHeight), (maxRight - rightHeight) ];
-
-    return [ leftWindow, rightWindow ];
+function calculateMax(height, left, right, maxLeft, maxRight) {
+    maxLeft = (height[left] > maxLeft) ? height[left] : maxLeft;
+    maxRight = (height[right] > maxRight) ? height[right] : maxRight;
+    return [maxLeft, maxRight];
 }
