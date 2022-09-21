dcbd88336309e436f1d5321051a7e2719d747e68
deepak kumar <deepakparamesh007@gmail.com>
2022-09-21 21:13:55 +0530

    Time: 115 ms (46.30%), Space: 43.9 MB (85.35%) - LeetHub

--- 57-insert-interval/57-insert-interval.js
+++ 57-insert-interval/57-insert-interval.js
@@ -4,31 +4,21 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-    let i = 0;
-  while (i < intervals.length) {
-    const [a, b] = intervals[i];
-    const [c, d] = newInterval;
-
-    if (
-      (a >= c && a <= d) ||
-      (b >= c && b <= d) ||
-      (c >= a && c <= b) ||
-      (d >= a && d <= b)
-    ) {
-      newInterval = mergeIntervals(intervals[i], newInterval);
-      intervals.splice(i, 1);
-    } else {
-      i++;
-    }
-  }
-
-  intervals.push(newInterval);
-  intervals.sort((a, b) => a[0] - b[0]);
-
-  return intervals;
+    let result = [];
+    for(let i=0; i<intervals.length; i++) {
+        // this condition is to check once the conflict is resolved
+        if(newInterval[1] < intervals[i][0]) {
+            result.push(newInterval);
+            return [...result, ...intervals.slice(i)];
+        } else if(newInterval[0] > intervals[i][1]) {
+            result.push(intervals[i]);
+        } else {
+            newInterval = [
+                Math.min(newInterval[0], intervals[i][0]),
+                Math.max(newInterval[1], intervals[i][1])
+            ]
+        }
+    };
+    result.push(newInterval);
+    return result;
 };
-
-const mergeIntervals = (int1, int2) => [
-  Math.min(int1[0], int2[0]),
-  Math.max(int1[1], int2[1]),
-];
