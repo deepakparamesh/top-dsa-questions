dbb3c5da66cf897465764d0c8350031d274b923d
deepak kumar <deepakparamesh007@gmail.com>
2022-09-23 23:29:09 +0530

    Time: 273 ms (92.29%), Space: 74.7 MB (62.51%) - LeetHub

---
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function(intervals) {
+    intervals.sort((a,b) => a[0] - b[0]);
+    let result  = 0;
+    let prevEnd = intervals[0][1];
+    
+    for(let i=1; i<intervals.length; i++){
+        if(intervals[i][0] >= prevEnd) {
+            prevEnd = intervals[i][1]
+        } else {
+            result +=1;
+            prevEnd = Math.min(intervals[i][1], prevEnd)
+        }
+    }
+    return result;
+};
