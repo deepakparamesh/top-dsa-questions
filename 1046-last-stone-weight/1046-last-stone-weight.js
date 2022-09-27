e170cf606c6df832d417d3349b4b5f8157677480
deepak kumar <deepakparamesh007@gmail.com>
2022-09-27 23:02:19 +0530

    Time: 122 ms (18.43%), Space: 42.9 MB (68.47%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -3,29 +3,23 @@
  * @return {number}
  */
 var lastStoneWeight = function (stones) {
-    const maxHeap = getMaxHeap(stones)
-
-    shrink(maxHeap)
-
-    return !maxHeap.isEmpty()
-        ? maxHeap.front().element
-        : 0
+    const maxHeap = getMaxHeap(stones);
+    shrink(maxHeap);
+    return !maxHeap.isEmpty() ? maxHeap.front().element : 0
 };
 
-const getMaxHeap = (stones, maxHeap = new MaxPriorityQueue()) => {
+const getMaxHeap = (stones) => {
+    let maxHeap = new MaxPriorityQueue();
     for (const stone of stones) {
         maxHeap.enqueue(stone)
     }
-
-    return maxHeap
+    return maxHeap;
 }
 
 const shrink = (maxHeap) => {
     while (1 < maxHeap.size()) {
         const [ x, y ] = [ maxHeap.dequeue().element, maxHeap.dequeue().element ]
         const difference = x - y;
-
-        const isPositive = 0 < difference
-        if (isPositive) maxHeap.enqueue(difference);
+        if (difference > 0) maxHeap.enqueue(difference);
     }
 }
