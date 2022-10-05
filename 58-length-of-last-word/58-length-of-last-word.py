fdc4fdbe656bffa88aed699ce0173a63569955d6
deepak kumar <deepakparamesh007@gmail.com>
2022-10-05 12:34:14 +0530

    Time: 51 ms (52.30%), Space: 14 MB (39.01%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -1,7 +1,3 @@
-# How do I detect the last word first, The delimiter is Space.
-# Instead of going from the front to back, we can count from the last of the array.
-# When we hit the first space, then we have detected a word. return the number of count that we have used.
-# edge case is : if there is any space at the end of the word.
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         
