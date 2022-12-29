286495d0616c6feb072423d257b18c07967d8cde
deepak kumar <deepakparamesh007@gmail.com>
2022-12-29 17:54:30 +0530

    Time: 2225 ms (18.25%), Space: 37.3 MB (6.72%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -4,15 +4,15 @@ class Solution:
         k = len(nums) - k
         
         def quickselect(l,r):
-            pivot, p = nums[r], l
+            pivot, pointer = nums[r], l
             for i in range(l, r):
                 if nums[i] <= pivot:
-                    nums[p], nums[i] = nums[i], nums[p]
-                    p += 1
-            nums[p], nums[r] = nums[r], nums[p]
+                    nums[pointer], nums[i] = nums[i], nums[pointer]
+                    pointer += 1
+            nums[pointer], nums[r] = nums[r], nums[pointer]
             
-            if p > k: return quickselect(l, p-1)
-            elif p < k: return quickselect(p+1, r)
-            else: return nums[p]
+            if pointer > k: return quickselect(l, pointer-1)
+            elif pointer < k: return quickselect(pointer+1, r)
+            else: return nums[pointer]
             
         return quickselect(0, len(nums) - 1)
