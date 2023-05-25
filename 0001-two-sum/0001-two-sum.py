d129bcfa5c48c8b629c5d4e4d8eb651f305c8cdd
deepak kumar <deepakparamesh007@gmail.com>
2023-05-25 09:30:52 +0530

    Time: 67 ms (73.83%), Space: 17.7 MB (16.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        previousMap = {} # value -> index
-        
-        for i, n in enumerate(nums):
-          difference = target - n
-          if difference in previousMap:
-            return [previousMap[difference], i]
-          previousMap[n] = i
+      
+      prevMap = {} # num -> index
+      
+      for i,n in enumerate(nums):
+        difference = target - n
+        if difference in prevMap:
+          return [prevMap[difference],i]
+        prevMap[n] = i
