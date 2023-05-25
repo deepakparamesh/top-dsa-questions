b765e3b7a4f2cec5711b6e2d12f441739a3224d3
deepak kumar <deepakparamesh007@gmail.com>
2023-05-25 09:21:50 +0530

    Time: 76 ms (15.20%), Space: 17 MB (19.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if(len(s) != len(t)):
+          return False
         
-      # if the length of two words are unequal, you can return false
+        sCount,tCount = {},{}
         
-      #  loop through s 
-      #   check if the character is availabe in the t,
-            # if True -> remove the character
-            # if False -> return false
-      # if it reaches the end of the loop, return True
-      
-      if len(s) != len(t):
-        return False
-      
-      sMap, tMap = {} , {}
-      
-      for i in range(len(s)):
-        sMap[s[i]] = 1+ sMap.get(s[i],0)
-        tMap[t[i]] = 1+ tMap.get(t[i],0)
-      
-      return sMap == tMap
+        for i in range(len(s)):
+          sCount[s[i]] = 1+sCount.get(s[i],0)
+          tCount[t[i]] = 1+tCount.get(t[i],0)
+        
+        return tCount == sCount
