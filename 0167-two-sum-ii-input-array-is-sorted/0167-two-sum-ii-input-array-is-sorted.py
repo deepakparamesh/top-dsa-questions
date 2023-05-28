e44c2ec5cff3d46d01132822f1a64c9203c422a2
deepak kumar <deepakparamesh007@gmail.com>
2023-05-28 22:29:32 +0530

    Time: 140 ms (42.20%), Space: 17.4 MB (9.10%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,9 +1,20 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}  # val -> index
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#         prevMap = {}  # val -> index
 
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff]+1, i+1]
-            prevMap[n] = i
+#         for i, n in enumerate(numbers):
+#             diff = target - n
+#             if diff in prevMap:
+#                 return [prevMap[diff]+1, i+1]
+#             prevMap[n] = i
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
