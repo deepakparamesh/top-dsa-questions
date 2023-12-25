871387b5f85097a271ea65e0d9a4b07542f3b9ab
Deepak Kumar <deepakparamesh007@gmail.com>
2023-12-25 22:01:03 +0530

    Time: 88 ms (93.57%), Space: 22.9 MB (5.48%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,22 +1,39 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        count = {}
+#         [1,1,1,2,2,3] K = 2
+        
+#         frequencyDictionary = {
+#             1:3
+#             2:2
+#             3:1
+#         }
+        
+#         Timecomplexity : O(n) * O(nlogn)
+#         SpaceComplexity: O(n)
+            
+#                   0   1   2  3  4  5   6
+#         bucket = [[],[3],[2],[1],[],[],[]]
+        
+#         result = [1,2]
+        
+#         O(n) + O(n) + O(nlen(bucket)) => O(n*m)
+        
+        frequencyDictionary = {}
         
         for num in nums:
-          count[num] = count.get(num,0) + 1
+            frequencyDictionary[num] = frequencyDictionary.get(num, 0) + 1
         
-        frequencyBucket = [[] for i in range(len(nums)+1)]
+        bucket = [[] for i in range(len(nums)+1)]
         
-        for num, value in count.items():
-          frequencyBucket[value].append(num)
-        
+        for key, value in frequencyDictionary.items():
+            bucket[value].append(key)
         
         result = []
-        for i in range(len(frequencyBucket)-1,0,-1):
-          for num in frequencyBucket[i]:
-            result.append(num)
-            if len(result) == k:
-              return result
-          
+        for topindex in range(len(bucket)-1,-1,-1):
+                if len(bucket[topindex]) > 0:
+                    for num in bucket[topindex]:
+                        if len(result) < k:
+                            result.append(num)
         
+        return result
