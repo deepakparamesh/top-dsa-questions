26d78cd6d669b49fe468a75be5e7a7e7d986f5b4
Deepak Kumar <deepakparamesh007@gmail.com>
2023-12-25 22:46:35 +0530

    Time: 4330 ms (33.79%), Space: 32.8 MB (15.47%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,15 +1,23 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+#         [100,4,200,1,3,2]
+        
+#         [1,2,3,4,100,200]
+#         O(nlogn)
+#         O(1)
+        
         numSet = set(nums)
-        longest = 0
+        globalLongest = 0
         
         for num in nums:
-          # To check if this is the beginning of a sequence
-          if (num-1) not in numSet:
-            currentLongest = 1
-            while num+currentLongest in numSet: # This is where you made mistake, you put num+1
-              currentLongest +=1
-            
-            longest = max(longest, currentLongest)
+            localLongest = 1
+            if num-1 not in numSet:
+                while num+localLongest in numSet:
+                    localLongest += 1
+            globalLongest = max(localLongest, globalLongest)
         
-        return longest
+        return globalLongest
+    
+                
+            
+        
