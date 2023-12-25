e03238762201dd4f9f040ac2572d5e14064400e8
Deepak Kumar <deepakparamesh007@gmail.com>
2023-12-25 20:19:31 +0530

    Time: 92 ms (83.39%), Space: 23.1 MB (7.65%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,21 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # declare a key value with value as array
-        result = collections.defaultdict(list)
         
-        for string in strs:
-          countHolder = [0]*26
-          for character in string:
-            # 101 - 97 = 4
-            countHolder[ord(character) - ord('a')] += 1
-          result[tuple(countHolder)].append(string)
+        result = []
+        anagramDictionary = {}
         
-        return result.values()
+        for str in strs:
+            hashcode = [0] * 26
+            
+            for char in str:
+                hashcode[ord(char) - ord('a')] += 1
+            
+            if tuple(hashcode) not in anagramDictionary :
+                anagramDictionary[tuple(hashcode)] = []
+                
+            anagramDictionary[tuple(hashcode)].append(str)    
+        
+        return anagramDictionary.values()
+    
+                
+        
