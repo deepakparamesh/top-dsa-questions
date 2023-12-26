7e682a7436be1eefb32ebb6856974207b7bfa074
Deepak Kumar <deepakparamesh007@gmail.com>
2023-12-26 18:16:50 +0530

    Time: 106 ms (32.68%), Space: 17.3 MB (7.75%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,21 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)  # key = (r /3, c /3)
-
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == ".":
+        columnDict = collections.defaultdict(set)
+        rowDict = collections.defaultdict(set)
+        squareDict = collections.defaultdict(set)
+        
+        for row in range(9):
+            for column in range(9):
+                currentNum = board[row][column]
+                if board[row][column] == ".":
                     continue
-                if (
-                    board[r][c] in rows[r]
-                    or board[r][c] in cols[c]
-                    or board[r][c] in squares[(r // 3, c // 3)]
+                
+                if(
+                    board[row][column] in rowDict[row] or
+                    board[row][column] in columnDict[column] or
+                    board[row][column] in squareDict[(row//3, column//3)]
                 ):
                     return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
-
+                
+                columnDict[column].add(board[row][column])
+                rowDict[row].add(board[row][column])
+                squareDict[(row//3,column//3)].add(board[row][column])
         return True
