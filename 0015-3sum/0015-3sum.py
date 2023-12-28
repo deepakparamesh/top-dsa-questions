5ababf6fadf5afcfa640f287a3caf0ff32008bf1
Deepak Kumar <deepakparamesh007@gmail.com>
2023-12-28 22:50:53 +0530

    Time: 1118 ms (51.77%), Space: 21.5 MB (16.79%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -25,11 +25,7 @@ class Solution:
                         left+=1
                 elif currentSum > 0:
                     right -= 1
-                    while left < right and nums[right] == nums[right+1]:
-                        right -= 1
                 elif currentSum < 0:
                     left += 1
-                    while  left < right and nums[left] == nums[left-1] :
-                        left+=1
             
         return result
