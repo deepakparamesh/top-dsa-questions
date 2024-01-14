8c73e69850dbbb3d7c011185be2e79ab7ba69a2d
Deepak Kumar <deepakparamesh007@gmail.com>
2024-01-14 15:56:30 +0530

    Time: 649 ms (98.32%), Space: 34.9 MB (19.64%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -4,20 +4,13 @@ class Solution:
         stack = [] # pair: [index, height]
         
         for index, height in enumerate(heights):
+            # The start index denotes from where the specific height starts, if the height starts from previous index.
             start = index
             while stack and stack[-1][1] > height:
                 stackIndex, stackHeight = stack.pop()
                 maxArea = max(maxArea, stackHeight * (index - stackIndex))
                 start = stackIndex
             stack.append((start, height))
-        
-        # for i, h in enumerate(heights):
-        #     start = i
-        #     while stack and stack[-1][1] > h:
-        #         index, height = stack.pop()
-        #         maxArea = max(maxArea, height * (i - index))
-        #         start = index
-        #     stack.append((start, h))
         
         # There will be few left over items in the stack, we need to calculate the area for them too and then recalculate the area with maxArea
         for index, height in stack:
