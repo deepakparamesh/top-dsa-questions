9a5562caef3baad22615526215acf5de47e13b82
Deepak Kumar <deepakparamesh007@gmail.com>
2024-02-01 19:24:42 +0530

    Time: 125 ms (96.00%), Space: 17.9 MB (75.30%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,16 +1,24 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
-        majoritySize = len(nums) // 2
-        hashMap = {}
+        # solution with extra space
+#         majoritySize = len(nums) // 2
+#         hashMap = {}
+        
+#         for num in nums:
+            
+#             hashMap[num] = hashMap.get(num, 0) + 1
+            
+#             if hashMap[num] > majoritySize:
+#                 return num
+        
+        
+        # 3 Boyer-moore algorithm without extra space
+        result, count = 0,0
         
         for num in nums:
-            
-            hashMap[num] = hashMap.get(num, 0) + 1
-            
-            if hashMap[num] > majoritySize:
-                return num
+            if count == 0:
+                result = num
+            count += (1 if num == result else -1)
         
-        
-        
-        
+        return result
