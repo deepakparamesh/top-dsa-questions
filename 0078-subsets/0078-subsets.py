0b9eb76ad850fcf12bc767281a6861737e127a53
Deepak Kumar <deepakparamesh007@gmail.com>
2024-06-21 13:49:53 +0530

    Time: 47 ms (6.00%), Space: 16.5 MB (99.52%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,23 +1,15 @@
-class Solution(object):
-    def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        res = []
-
-        subset = []
-
-        def dfs(i):
-            if i >= len(nums):
-                res.append(subset[:])
-                return
-            # decision to include nums[i]
-            subset.append(nums[i])
-            dfs(i + 1)
-            # decision NOT to include nums[i]
-            subset.pop()
-            dfs(i + 1)
-
-        dfs(0)
-        return res
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        
+        def backtrack(start, path):
+            result.append(path[:])
+            
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                backtrack(i+1, path)
+                path.pop()
+             
+        backtrack(0,[])
+        
+        return result
