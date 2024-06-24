cd0cf127d7da09208d146eff2d5d0d903170806e
Deepak Kumar <deepakparamesh007@gmail.com>
2024-06-24 16:05:08 +0530

    Time: 927 ms (64.20%), Space: 21.9 MB (93.69%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -2,14 +2,19 @@ class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if sum(gas) < sum(cost):
             return -1
-    
-        start, total_gas, tank = 0, 0, 0
+
+        total_tank, current_tank = 0, 0
+        starting_station = 0
 
         for i in range(len(gas)):
-            tank += gas[i] - cost[i]
-            total_gas += gas[i] - cost[i]
-            if tank < 0:
-                start = i + 1
-                tank = 0
+            total_tank += gas[i] - cost[i]
+            current_tank += gas[i] - cost[i]
 
-        return start if total_gas >= 0 else -1
+            # If one couldn't get here
+            if current_tank < 0:
+                # Pick up the next station as the starting one
+                starting_station = i + 1
+                # Reset current tank
+                current_tank = 0
+
+        return starting_station if total_tank >= 0 else -1
