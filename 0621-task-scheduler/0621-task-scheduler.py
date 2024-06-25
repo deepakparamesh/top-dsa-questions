a535be36eb81aa9a6062b44d180c9cab6b36adfc
Deepak Kumar <deepakparamesh007@gmail.com>
2024-06-25 19:09:03 +0530

    Time: 342 ms (96.82%), Space: 17.1 MB (24.30%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,24 +1,8 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        if n == 0:
-            return len(tasks)
-        
         task_counts = Counter(tasks)
-        max_heap = [-count for count in task_counts.values()]
-        heapq.heapify(max_heap)
-        
-        time = 0
-        queue = deque()  # Queue to keep track of cooling tasks (time, count)
-        
-        while max_heap or queue:
-            time += 1
-            
-            if max_heap:
-                count = heapq.heappop(max_heap) + 1  # Remove most frequent task and decrement its count
-                if count:  # If there are remaining tasks, add to the cooldown queue
-                    queue.append((time + n, count))
-                    
-            if queue and queue[0][0] == time:  # Check if a task is ready to be scheduled again
-                heapq.heappush(max_heap, queue.popleft()[1])
-        
-        return time
+        max_freq = max(task_counts.values())
+        max_freq_count = list(task_counts.values()).count(max_freq)
+
+        intervals = (max_freq - 1) * (n + 1) + max_freq_count
+        return max(len(tasks), intervals)
