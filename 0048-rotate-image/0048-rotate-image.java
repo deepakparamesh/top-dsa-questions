196c6f625c9f3ec288b9cdfd881bbcdba4f5a9e8
Deepak Kumar <deepakparamesh007@gmail.com>
2025-06-26 20:14:07 +0530

    Time: 0 ms (100%), Space: 42.4 MB (41.59%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                // Swap elements at [i][j] and [j][i]
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                // Swap elements in the row
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
+
+
+// 1 2 3
+// 4 5 6
+// 7 8 9
+
+// 1 4 7
+// 2 5 8
+// 3 6 9
+
+// 7 4 1
+// 8 5 2
+// 9 6 3
+
+
