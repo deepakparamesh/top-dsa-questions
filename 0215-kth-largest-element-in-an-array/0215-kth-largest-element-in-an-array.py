6a3b84ed9629df7179a24665bd2b379857f47923
Deepak Kumar <deepakparamesh007@gmail.com>
2025-09-27 16:04:24 +0530

    Time: 38 ms (95.77%), Space: 28.6 MB (81.98%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,18 +1,64 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         
-        k = len(nums) - k
+        min_heap = nums[:k]
+        heapq.heapify(min_heap)
+
+        for num in nums[k:]:
+            if num > min_heap[0]:
+                heapq.heappushpop(min_heap, num)
         
-        def quickselect(l,r):
-            pivot, pointer = nums[r], l
-            for i in range(l, r):
-                if nums[i] <= pivot:
-                    nums[pointer], nums[i] = nums[i], nums[pointer]
-                    pointer += 1
-            nums[pointer], nums[r] = nums[r], nums[pointer]
+        return min_heap[0]
+
+        # def quickselect(left, right, k_smallest):
+
+        #     if left == right:
+        #         return nums[left]
             
-            if pointer > k: return quickselect(l, pointer-1)
-            elif pointer < k: return quickselect(pointer+1, r)
-            else: return nums[pointer]
+        #     pivot_index = random.randint(left, right)
+
+        #     pivot_index = partition(left, right, pivot_index)
+
+        #     left_count = pivot_index - left
+
+        #     if k_smallest <= left_count:
+        #         return quickselect(left, pivot_index -1, k_smallest)
+        #     elif k_smallest == left_count + 1:
+        #         return nums[pivot_index]
+        #     else:
+        #         return quickselect(pivot_index +1, right, k_smallest - left_count - 1)
+        
+        # def partition(left, right, pivot_index):
+
+        #     pivot_value = nums[pivot_index]
+
+        #     nums[pivot_index], nums[right] = nums[right], nums[pivot_index]
+
+        #     store_index = left
+        #     for i in range(left, right):
+        #         if nums[i] < pivot_value:
+        #             nums[store_index], nums[i] = nums[i], nums[store_index]
+        #             store_index += 1
             
-        return quickselect(0, len(nums) - 1)
+        #     nums[right], nums[store_index] = nums[store_index], nums[right]
+
+        #     return store_index
+    
+        # n = len(nums)
+        # return quickselect(0,n-1,n-k+1)
+
+# [3,2,1,5,6,4]
+# K=2 
+
+# [3,2,1,5,6,4]
+
+# Elements > 4 : [5, 6] -> go to left
+# 4: [3,2,1,4] -> go to right
+
+# [5,6, 3,2,1,4]
+
+# current array: [5,6]
+# Element > 6: []
+# Element <= 
+
+
