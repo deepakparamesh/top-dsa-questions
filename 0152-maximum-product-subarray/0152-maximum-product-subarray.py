5634db12a9fd94e41c393cc1287b91ca8f389f73
Deepak Kumar <deepakparamesh007@gmail.com>
2025-10-09 17:51:23 +0530

    Time: 11 ms (50.08%), Space: 18.5 MB (37.51%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,32 +1,40 @@
 class Solution:
-    def maxProduct(self, arr: List[int]) -> int:
-#         prefix, suffix = 1,1
+    def maxProduct(self, nums: List[int]) -> int:
         
-#         size = len(nums)
-#         result = float("-inf")
-#         for index in range(size):
-#             if prefix == 0:
-#                 prefix = 1
-#             if suffix == 0:
-#                 suffix = 0
-            
-#             prefix *= nums[index]
-#             suffix *= nums[size - index - 1]
-            
-#             result = max(result, max(prefix, suffix))
+        if not nums:
+            return 0
         
-#         return result
-        n = len(arr) # size of array.
+        max_so_far = nums[0]
+        max_ending_here = nums[0]
+        min_ending_here = nums[0]
 
-        pre, suff = 1, 1
-        ans = float('-inf')
-        for i in range(n):
-            if pre == 0:
-                pre = 1
-            if suff == 0:
-                suff = 1
-            pre *= arr[i]
-            suff *= arr[n - i - 1]
-            ans = max(ans, max(pre, suff))
-        return ans
-            
+        for i in range(1, len(nums)):
+            current=nums[i]
+
+            temp_max = max(current, max_ending_here * current, min_ending_here * current)
+
+            min_ending_here = min(current, max_ending_here* current, min_ending_here*current)
+
+            max_ending_here = temp_max
+
+            max_so_far = max(max_so_far, max_ending_here)
+    
+        return max_so_far
+
+
+
+
+# 0: num = 2
+# [2,3,-2,4]
+# max=2
+# min=2
+# global_max=2
+
+# 1: num=3
+# max = max(3,2*3) = 6
+# min = min(3,2*3)= 3
+# global_max = 6
+# 2 : -2
+# max = max(-2,6*(-2),3*(-2)) = -2
+# min = min(-2, 6*-2, 3*-2)=-12
+# global_max = 6
